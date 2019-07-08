3f3b2b84974ea11c5bb7bb1eafa0952a948a1c70
Jesco Christ <jesco@milter.intranet>
2019-07-08 02:11:31 +0200

    Added package for support classes.

---
+++ src/de/jescochrist/waypoint/support/WaypointSupport.java
@@ -0,0 +1,8 @@
+package de.jescochrist.waypoint.support;
+
+public class WaypointSupport {
+	
+	// Put things from classes related to setting and getting waypoint data here to
+	// shrink down command/scheduler classes.
+	
+}
